81cabf3a0370d79df879277362fdc56fc381ac1c
Sandeep Parikh <sandeep@clusterbeep.org>
2011-10-13 16:18:04 -0500

    some simple views and templates for tags

--- webapp/tags/views.py
+++ webapp/tags/views.py
@@ -1,1 +1,21 @@
-# Create your views here.
+from django.shortcuts import render_to_response
+from django.http import HttpResponseRedirect, HttpResponse
+from django.template import RequestContext
+
+from tagging.models import Tag, TaggedItem
+
+from datasets.models import Dataset
+
+def index(request):
+    tags = Tag.objects.usage_for_model(Dataset, counts=True)
+    return render_to_response('tags/index.html', {
+        'tags':tags,
+    }, context_instance=RequestContext(request))
+
+def view_tag(request, tag_name):
+    datasets = TaggedItem.objects.get_by_model(Dataset, tag_name)
+    return render_to_response('tags/view_tag.html', {
+        'tag_name':tag_name,
+        'datasets':datasets,
+    }, context_instance=RequestContext(request))
+    
