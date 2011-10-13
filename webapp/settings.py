b7e0acb5a114a793d6ae74e00de0860a67bf2d35
Sandeep Parikh <sandeep@clusterbeep.org>
2011-10-13 16:20:52 -0500

    updated static path for dotcloud

--- webapp/settings.py
+++ webapp/settings.py
@@ -67,7 +67,7 @@ STATIC_ROOT = '/home/dotcloud/data/static/'
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
-STATIC_URL = '/static/'
+STATIC_URL = 'http://www.opensciencedata.org/static/'
 
 # URL prefix for admin static files -- CSS, JavaScript and images.
 # Make sure to use a trailing slash.
