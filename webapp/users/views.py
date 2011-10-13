71a50e29b8e726e190cf55a27b2d6b04cd8fa519
Sandeep Parikh <sandeep@clusterbeep.org>
2011-10-13 16:21:10 -0500

    empty user views for now

--- webapp/users/views.py
+++ webapp/users/views.py
@@ -1,1 +1,10 @@
-# Create your views here.
+from django.shortcuts import render_to_response
+from django.http import HttpResponseRedirect, HttpResponse
+from django.template import RequestContext
+from datasets.models import Dataset
+
+def index(request):
+    pass
+
+def view_user(request, username):
+    pass
