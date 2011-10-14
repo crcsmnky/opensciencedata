4f19f386c407ae07288d9328be8385a16eb2319c
Sandeep Parikh <sandeep@clusterbeep.org>
2011-10-13 23:16:44 -0500

    cleaned up and added a view_user view and template

--- webapp/users/urls.py
+++ webapp/users/urls.py
@@ -1,6 +1,5 @@
 from django.conf.urls.defaults import patterns, include, url
 
 urlpatterns = patterns('webapp.users.views',
-    url(r'^$', 'index'),
     url(r'^(?P<username>[0-9a-z]+)$', 'view_user'),
 )
--- webapp/users/views.py
+++ webapp/users/views.py
@@ -1,11 +1,19 @@
 from django.shortcuts import render_to_response
 from django.http import HttpResponseRedirect, HttpResponse
 from django.template import RequestContext
+from django.contrib.auth.models import User
 
 from webapp.datasets.models import Dataset
 
-def index(request):
-    pass
-
 def view_user(request, username):
-    pass
+    try:
+        user = User.objects.get(username=username)
+    except User.DoesNotExist:
+        user = None
+
+    datasets = Dataset.objects.filter(user=user).order_by('downloads')
+
+    return render_to_response('users/view_user.html', {
+        'user':user,
+        'datasets':datasets,
+    }, context_instance=RequestContext(request))
